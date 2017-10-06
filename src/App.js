810a371d4db813f6f5a1f8ea8882cbbff0a2cccc
Joscelyn James <josce.james7@hotmail.com>
2017-10-06 15:38:31 -0600

    [component][state][props] created a component with a prop that changes the state as you type in the input box

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// Commit 1
 
 class App extends React.Component {
   constructor(){
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<
-	App car={5} txt="this is the prop" />, 
+	App txt="this is the prop" />, 
 	document.getElementById('root')
 );
 
