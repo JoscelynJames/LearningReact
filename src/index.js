4074ba9f61b73ca6f6026b33fb5073a556bca7ea
Joscelyn James <josce.james7@hotmail.com>
2017-10-06 15:25:51 -0600

    [react][component][state] created a react component with a input that changes the state

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<
+	App car={5} txt="this is the prop" />, 
+	document.getElementById('root')
+);
+
